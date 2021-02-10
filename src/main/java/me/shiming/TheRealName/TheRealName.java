5d24ca93b6afa44455c0590aaca9d1393beb0dd7
shiming-shimu <3352711163@qq.com>
2021-02-10 10:38:03 +0800

    Making🛠🛠🛠

---
+++ src/main/java/me/shiming/TheRealName/TheRealName.java
@@ -0,0 +1,17 @@
+package me.shiming.TheRealName;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TheRealName extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
